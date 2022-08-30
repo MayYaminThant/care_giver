bf37faf674fc4cc354bcf5aa5e46ba06365ae734
MayYaminThant <mayyamin17420@gmail.com>
2022-08-30 17:34:07 +0630

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyMainCareDiver());
+}
+
+class MyMainCareDiver extends StatefulWidget {
+  const MyMainCareDiver({Key? key}) : super(key: key);
+
+  @override
+  State<MyMainCareDiver> createState() => _MyMainCareDiverState();
+}
+
+class _MyMainCareDiverState extends State<MyMainCareDiver> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
